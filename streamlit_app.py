8bb7ffe74ef480567eb52fc0d02da64fd7396576
hash17-hub <hash17@protonmail.com>
2025-07-24 16:07:49 +0100

    Update streamlit_app.py
    Connected changes needed bc of move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,16 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session
 
 # Write directly to the app
 st.title(f"My First Streamlit App :cup_with_straw: {st.__version__}")
-# st.write(
-#   """Replace this example with your own code!
-#   **And if you're new to Streamlit,** check
-#   out our easy-to-follow guides at
-#   [docs.streamlit.io](https://docs.streamlit.io).
-#   """
-# )
 
 from snowflake.snowpark.functions import col, when_matched
 
-# ---------------------------------------------------
-name_on_order = st.text_input("Name on Smoothie:")
-st.write("The name on your Smooothie will be:", name_on_order)
-#----------------------------------------------------
+cnx = st.connection("snowflake")
+session = cnx.session()
 
-session = get_active_session()
+# session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
@@ -28,6 +19,7 @@ ingredients_string = ''
 ingredients_list = st.multiselect(
 'Choose up to 5 ingredients:',my_dataframe, max_selections=5
 )
+
 if ingredients_list:    
     for fruit_shosen in ingredients_list:
         ingredients_string += fruit_shosen + ' '
@@ -43,3 +35,4 @@ stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
 if time_to_insert:
     session.sql(stmt).collect()
     st.success('Your Smoothie is ordered!', icon="✅")
+    
